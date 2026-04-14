12aa05ed3d6617a654c447fe7076eea6c6b69449
Priya Narayan  Devadig <devadigpriya1@gmail.com>
2026-04-14 21:49:29 +0530

    function added to release seats

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -7,7 +7,7 @@ export const stripeWebhooks=async(request,response)=>{
     let event ;
 
     try {
-        event=stripe.webhooks.constructEvent(request.body,sig,process.env.STRIPE_WEBHOOK_SECRET);
+        event=stripeInstance.webhooks.constructEvent(request.body,sig,process.env.STRIPE_WEBHOOK_SECRET);
     } catch (error) {
         return response.status(400).send(`Webhook Error:${error.message}`);
     }
